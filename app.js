155169b1eda64fbd40ae22805c1b6a03f6e7f5b8
Indocpa_Asbin <wisnu@indocpa.co.id>
2017-01-03 03:07:25 +0000

    2

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ app.post('/webhook', function (req, res) {
         if (messagingEvent.optin) {
           receivedAuthentication(messagingEvent);
         } else if (messagingEvent.message) {
-          receivedMessage(messagingEvent);
+		  receivedMessage(messagingEvent);
         } else if (messagingEvent.delivery) {
           receivedDeliveryConfirmation(messagingEvent);
         } else if (messagingEvent.postback) {
--- library/receiver/receivedMessage.js
+++ library/receiver/receivedMessage.js
@@ -105,7 +105,7 @@ module.exports = function receivedMessage(event) {
             break;
 
         default:
-            sendTextMessage(senderID, messageText, () => {});
+            // sendTextMessage(senderID, messageText, () => {});
         }
     } else if (messageAttachments) {
         sendTextMessage(senderID, 'Message with attachment received', () => {});
